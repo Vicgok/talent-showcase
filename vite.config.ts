6c9c81b7afdb6a4c5e2b65bedfc96aa5c30473f5
Vigneshwaraa <vickygogeta1@gmail.com>
2026-05-26 23:58:27 +0530

    fix: architecture corrupted with vite and vinxi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server", port: 8000 },
-  },
+  plugins: [tsConfigPaths(), tailwindcss(), tanstackStart(), react()],
 });
